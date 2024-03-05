76635edbb3b8ea92bbda3c69809a46cf1f7aa2b3
SoporteS1st3m4as2 <150272012+SoporteS1st3m4as2@users.noreply.github.com>
2024-03-05 17:10:28 -0600

    Update InydeForm.js

--- src/components/Admin/Inyde/InydeForm/InydeForm.js
+++ src/components/Admin/Inyde/InydeForm/InydeForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Form, Dropdown, Input, Container} from "semantic-ui-react";
+import {Form, Dropdown, Input, GridRow, GridColumn, Grid, Segment} from "semantic-ui-react";
 import { useFormik } from 'formik';
 import {initialValues, validationSchema} from "./InydeForm.form"
 import { Mag } from '../../../../api';
@@ -41,11 +41,101 @@ export function InydeForm(props) {
     }
   });
   return (
-    <Form className='form-iynde' onSubmit={formik.handleSubmit}>
-    <Form.Group className='form-iynde__asesor' widths='equal'>
-      <Form.TextArea name="activos" placeholder="activos" onChange={formik.handleChange} value={formik.values} error={formik.errors.activos}/>
-    </Form.Group>
-    <Form.Input name='folio_IyD'/>
-    </Form>
+    <>
+    <Grid columns={1}>
+      <GridRow>
+        <GridColumn>
+        <Grid columns={2}>
+      <GridRow>
+        <GridColumn>
+          ID
+          <Segment>{mag.folio_IyD}</Segment>
+        </GridColumn>
+        <GridColumn>
+          CLAVE FÓRMULA
+          <Segment></Segment>
+        </GridColumn>
+      </GridRow>
+    </Grid>
+    <Grid columns={2}>
+      <GridRow>
+        <GridColumn>
+          ASESOR
+          <Segment>{mag.asesor}</Segment>
+        </GridColumn>
+        <GridColumn>
+          MÉDICO
+          <Segment>{mag.cardcode}</Segment>
+        </GridColumn>
+      </GridRow>
+    </Grid>
+    <Grid columns={2}>
+      <GridRow>
+        <GridColumn>
+          ACTIVOS
+          <Segment>{mag.activos}</Segment>
+        </GridColumn>
+        <GridColumn>
+          BASE
+          <Segment>{mag.base}</Segment>
+        </GridColumn>
+      </GridRow>
+    </Grid>
+        </GridColumn>
+      </GridRow>
+      <GridRow>
+        <Grid columns={3}>
+          <GridRow>
+            <GridColumn>
+            ESPECIALIDAD
+          <Segment>{mag.especialidad}</Segment>
+            </GridColumn>
+            <GridColumn>
+            CLASIFICACIÓN
+          <Segment>{mag.clasi}</Segment>
+            </GridColumn>
+            <GridColumn>
+            PADECIMIENTO
+          <Segment>{mag.padecimiento}</Segment>
+            </GridColumn>
+          </GridRow>
+        </Grid>
+        <Grid columns={3}>
+          <GridRow>
+            <GridColumn>
+            RECETA
+          <Segment>{mag.receta}</Segment>
+            </GridColumn>
+            <GridColumn>
+            EXCLUSIVO
+          <Segment>{mag.excl}</Segment>
+            </GridColumn>
+            <GridColumn>
+            REFRIGERACIÓN
+          <Segment>{mag.refri}</Segment>
+            </GridColumn>
+          </GridRow>
+        </Grid>
+      </GridRow>
+      <GridRow>
+        <Grid columns={3}>
+          <GridRow>
+            <GridColumn>
+            FÓRMULA EXISTENTE
+          <Segment>{mag.existe}</Segment>
+            </GridColumn>
+            <GridColumn>
+            CLAVE
+          <Segment>{mag.clave_ex}</Segment>
+            </GridColumn>
+            <GridColumn>
+            BASE
+          <Segment>{mag.base_ex}</Segment>
+            </GridColumn>
+          </GridRow>
+        </Grid>
+      </GridRow>
+      </Grid></>
+  
   )
 }
