bec66680e8f8c739f493505e34b15aa4d5ff99e5
SoporteS1st3m4as2 <150272012+SoporteS1st3m4as2@users.noreply.github.com>
2024-03-21 15:17:11 -0600

    Update InydeForm.js

--- src/components/Admin/Inyde/InydeForm/InydeForm.js
+++ src/components/Admin/Inyde/InydeForm/InydeForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {GridRow, GridColumn, Grid, Segment, Form, FormCheckbox, Checkbox, Input, FormButton, Dropdown} from "semantic-ui-react";
-import { useFormik } from 'formik';
+import {Form, FormCheckbox, Checkbox, Input, FormButton, Dropdown, FormInput, FormGroup} from "semantic-ui-react";
+import { FormikConsumer, useFormik } from 'formik';
 import {initialValues, validationSchema} from "./InydeForm.form"
 import { Mag } from '../../../../api';
 import { useAuth } from '../../../../hooks';
@@ -10,13 +10,13 @@ const magController = new Mag();
 const handleChange=()=>{}
 
 const clasificacion = [
-  {key: "FM", Text:"FM", value:"FM"},
-  {key: "DC", Text:"DC", value:"DC"}
+  {key:"FM", text:"FM", value:"FM"},
+  {key:"DC", text:"DC", value:"DC"}
 ];
 
 const TipoF = [
-  {key: "Cerrada", Text:"Cerrada", value:"cd"},
-  {key: "Atributos", Text:"Atributos", value:"at"}
+  {key:"Cerrada", text:"Cerrada", value:"cd"},
+  {key:"Atributos", text:"Atributos", value:"at"}
 ];
 
 
@@ -28,7 +28,7 @@ export function InydeForm(props) {
     initialValues: initialValues(mag),
     validationSchema: validationSchema(mag),
     validateOnChange: false,
-    onSubmit: async(formValue)=>{
+    onSubmit: async (formValue) => {
       try {
         if(!mag){
           await magController.createMag(accessToken, formValue);
@@ -42,135 +42,47 @@ export function InydeForm(props) {
       }
     }
   });
+
   return (
-    <>
-    <Form>
-      <Segment>
-    <Grid columns={1} onSubmit={formik.handleSubmit}>
-      <GridRow>
-        <GridColumn>
-        <Grid columns={2}>
-      <GridRow>
-        <GridColumn>
-          ID<Segment>{mag.folio_IyD}</Segment>
-        </GridColumn>
-        <GridColumn>
-          ASESOR<Segment>{mag.asesor}</Segment>
-        </GridColumn>
-      </GridRow>
-    </Grid>
-    <Grid columns={2}>
-      <GridRow>
-        <GridColumn>
-          MÉDICO<Segment>{mag.cardcode}</Segment>
-        </GridColumn>
-        <GridColumn>
-          MUESTRA
-          <Segment><FormCheckbox name="necesita_muestra" onChange={formik.handleChange} value={formik.values.necesita_muestra} error={formik.errors.necesita_muestra}/></Segment>
-        </GridColumn>
-      </GridRow>
-    </Grid>
-    <Grid columns={2}>
-      <GridRow>
-        <GridColumn>
-          ACTIVOS
-          <Form.Input name='activos' onChange={formik.handleChange} value={formik.values.activos} error={formik.errors.activos}/>
-        </GridColumn>
-        <GridColumn>
-          BASE
-          <Form.Input name='base' onChange={formik.handleChange} value={formik.values.base} error={formik.errors.base}/>
-        </GridColumn>
-      </GridRow>
-    </Grid>
-        </GridColumn>
-      </GridRow>
-      <GridRow>
-        <Grid columns={3}>
-          <GridRow>
-            <GridColumn>
-            ESPECIALIDAD
-            <Form.Input name='especialidad' placeholder='7.5 ML' onChange={formik.handleChange} value={formik.values.especialidad} error={formik.errors.especialidad}/>
-            </GridColumn>
-            <GridColumn>
-            CLASIFICACIÓN
-            <Dropdown placeholder="Clasificaciones" fluid selection options={TipoF} onChange={(_,data)=>formik.setFieldValue("clasificacion",data.value)} value={formik.values.clasificacion} error={formik.errors.clasificacion}/>
-            </GridColumn>
-            <GridColumn>
-            PADECIMIENTO
-            <Form.Input name='padecimiento' onChange={formik.handleChange} value={formik.values.padecimiento} error={formik.errors.padecimiento}/>
-            </GridColumn>
-          </GridRow>
-        </Grid>
-        <Grid columns={3}>
-          <GridRow>
-            <GridColumn>
-            RECETA<Segment><FormCheckbox name="receta" onChange={formik.handleChange} value={formik.values.receta} error={formik.errors.receta}/></Segment>
-            </GridColumn>
-            <GridColumn>
-            EXCLUSIVO<Segment><FormCheckbox name="excl" onChange={formik.handleChange} value={formik.values.excl} error={formik.errors.excl}/></Segment>
-            </GridColumn>
-            <GridColumn>
-            REFRIGERACIÓN<Segment><FormCheckbox name="refri" onChange={formik.handleChange} value={formik.values.refri} error={formik.errors.refri}/></Segment>
-            </GridColumn>
-          </GridRow>
-        </Grid>
-      </GridRow>
-      <GridRow>
-        <Grid columns={3}>
-          <GridRow>
-            <GridColumn>
-            EXISTE<Segment><FormCheckbox name="existe" onChange={formik.handleChange} value={formik.values.existe} error={formik.errors.existe}/></Segment>
-            </GridColumn>
-            <GridColumn>
-            CLAVE<Segment>{mag.clave_ex}</Segment>
-            </GridColumn>
-            <GridColumn>
-            BASE<Segment>{mag.base_ex}</Segment>
-            </GridColumn>
-          </GridRow>
-        </Grid>
-      </GridRow>
-      <GridRow>
-        <Grid columns={2}>
-          <GridRow>
-            <GridColumn>
-            TIPO DE FÓRMULA
-            <select name='tipoF' onChange={formik.handleChange} value={formik.values.tipoF} error={formik.errors.tipoF}></select>
-            </GridColumn>
-            <GridColumn>
-            CADUCIDAD
-            <Form.Input name='caducidad' onChange={formik.handleChange} value={formik.values.caducidad} error={formik.errors.caducidad}/>
-            </GridColumn>
-          </GridRow>
-        </Grid>
-      </GridRow>
-      <GridRow>
-        <Grid columns={1}>
-          <GridRow>
-            <GridColumn>
-            INFORMACIÓN DEL DESARROLLO
-            <Form.Input name='infoDes' onChange={formik.handleChange} value={formik.values.infoDes} error={formik.errors.infoDes}/>
-            </GridColumn>
-          </GridRow>
-        </Grid>
-      </GridRow>
-      <GridRow>
-        <Grid columns={2}>
-          <GridRow>
-            <GridColumn>
-            COMENTARIOS INTERNOS
-            <Form.Input name='comInt' onChange={formik.handleChange} value={formik.values.comInt} error={formik.errors.comInt}/>
-            </GridColumn>
-            <GridColumn>
-            COMENTARIOS CLIENTES
-            <Form.Input name='comClie' onChange={formik.handleChange} value={formik.values.comClie} error={formik.errors.comClie}/>
-            </GridColumn>
-          </GridRow>
-        </Grid>
-      </GridRow>
-      </Grid>
-      </Segment>
-      <FormButton>Guardar</FormButton>
-      </Form></>
+    <Form onSubmit={formik.handleSubmit}>
+      <FormGroup widths='equal'>
+        <Form.Input name="folio_IyD" label="ID" onChange={formik.handleChange} value={formik.values.folio_IyD} error={formik.errors.folio_IyD}/>
+        <Form.Input name="asesor" label="Asesor" onChange={formik.handleChange} value={formik.values.asesor} error={formik.errors.asesor}/>
+      </FormGroup>
+      <FormGroup widths='equal'>
+        <Form.Input name="cardcode" label="Médico" onChange={formik.handleChange} value={formik.values.cardcode} error={formik.errors.cardcode}/>
+        <Form.Checkbox name='necesita_muestra' label="Necesita muestra" onChange={(_,data)=>formik.setFieldValue("necesita_muestra",data.checked)} checked={formik.values.necesita_muestra} error={formik.errors.necesita_muestra}/>
+      </FormGroup>
+      <FormGroup widths='equal'>
+        <Form.Input name="activos" label="Activos" onChange={formik.handleChange} value={formik.values.activos} error={formik.errors.activos}/>
+        <Form.Input name="base" label="Base" onChange={formik.handleChange} value={formik.values.base} error={formik.errors.base}/>
+      </FormGroup>
+      <FormGroup widths='equal'>
+        <Form.Input name="especialidad" label="Especialidad" onChange={formik.handleChange} value={formik.values.especialidad} error={formik.errors.especialidad}/>
+        <Dropdown placeholder="clasificacion" fluid selection options={clasificacion} onChange={(_,data)=>formik.setFieldValue("clasi",data.value)} value={formik.values.clasi} error={formik.errors.clasi}/>      
+        <Form.Input name="padecimiento" label="Padecimiento" onChange={formik.handleChange} value={formik.values.padecimiento} error={formik.errors.padecimiento}/>
+      </FormGroup>
+      <FormGroup widths='equal'>
+        <Form.Checkbox name="receta" label="receta" onChange={(_,data)=>formik.setFieldValue("receta",data.checked)} checked={formik.values.receta} error={formik.errors.receta}/>
+        <Form.Checkbox name="excl" label="Exclusivo" onChange={(_,data)=>formik.setFieldValue("excl",data.checked)} checked={formik.values.excl} error={formik.errors.excl}/>
+        <Form.Checkbox name="refri" label="Refrigeración" onChange={(_,data)=>formik.setFieldValue("refri",data.checked)} checked={formik.values.refri} error={formik.errors.refri}/>
+      </FormGroup>
+      <FormGroup widths='equal'>
+        <Form.Checkbox name='existe' label="Existe" onChange={(_,data)=>formik.setFieldValue("existe",data.checked)} checked={formik.values.existe} error={formik.errors.existe}/>
+        <Form.Input name="clave_ex" label="Clave" onChange={formik.handleChange} value={formik.values.clave_ex} error={formik.errors.clave_ex}/>
+        <Form.Input name="base_ex" label="Base" onChange={formik.handleChange} value={formik.values.base_ex} error={formik.errors.base_ex}/>
+      </FormGroup>
+      <FormGroup widths='equal'>
+        <Dropdown placeholder="TipoF" fluid selection options={TipoF} onChange={(_,data)=>formik.setFieldValue("tipoF",data.value)} value={formik.values.tipoF} error={formik.errors.tipoF}/>
+        <Form.Input name="caducidad" label="Caducidad" onChange={formik.handleChange} value={formik.values.caducidad} error={formik.errors.caducidad}/>
+      </FormGroup>
+      <FormButton type='submit' 
+      primary 
+      fluid 
+      loading={formik.isSubmitting} 
+      onClick={formik.handleSubmit}
+      >
+      </FormButton>
+      </Form>
   )
 }
