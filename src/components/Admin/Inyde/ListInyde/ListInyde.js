e5d406016356089002ba3dbc41ea41a42221ccbf
DoporteS1st3m4s <soporte.sistemas@o-lab.mx>
2024-03-25 23:02:30 -0600

    V.0.30

--- src/components/Admin/Inyde/ListInyde/ListInyde.js
+++ src/components/Admin/Inyde/ListInyde/ListInyde.js
@@ -1,73 +1,80 @@
-import React, {useEffect, useState} from 'react';
-import {Mag} from "../../../../api";
-import { size, map, filter } from 'lodash';
-import { Loader, Pagination, Form, Search} from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import { Mag } from '../../../../api';
+import { size, map } from 'lodash';
+import { Loader, Pagination, Search } from 'semantic-ui-react';
 import { InydeItem } from '../InydeItem/InydeItem';
 
 const magController = new Mag();
 
-export function ListInyde(props){
-    const {reload, onReload, onClose} = props;
-    const [mags, setMags] = useState(false)
-    const [page, setPage] = useState(1)
-    const [pagination, setPagination] = useState()
-    const [searchQuery, setSearchQuery] = useState('');
+export function ListInyde(props) {
+  const { reload, onReload, onClose } = props;
+  const [mags, setMags] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pagination, setPagination] = useState({});
+  const [searchQuery, setSearchQuery] = useState('');
 
-    useEffect(() => {
-      (async () =>{
-        try {
-            const response = await magController.getMag({page, limit: 10})     
-            setMags(response.docs);
-            setPagination({
-                limit: response.limit,
-                page: response.page,
-                pages: response.pages,
-                total: response.totalPages,
-            });
-        } catch (error) {
-            console.error(error)
-        }
-      })()
-    }, [page, reload])
-
-    const changePage=(_,data)=>{
-        setPage(data.activePage)
-    }
-
-    const handleSearchChange = (event, {value }) => {
-      setSearchQuery(value);
+  useEffect(() => {
+    const fetchMags = async () => {
+      try {
+        const response = await magController.getMag({ page, limit: 10 });
+        setMags(response.docs);
+        setPagination({
+          limit: response.limit,
+          page: response.page,
+          pages: response.pages,
+          total: response.totalPages,
+        });
+      } catch (error) {
+        console.error(error);
+      }
     };
 
-    const filteredMags = searchQuery
-  ? filter(mags, mag => mag._id.includes(searchQuery))
-  : mags;
-    
-    if(!mags) return <Loader active inline="centered"/>
-    if(size(mags)===0)return "No hay cotizaciones";
+    fetchMags();
+  }, [page, reload]);
 
+  const changePage = (_, data) => {
+    setPage(data.activePage);
+  };
+
+  const handleSearchChange = (event, { value }) => {
+    setSearchQuery(value.toLowerCase());
+  };
+
+  const filteredMags = mags.filter((mag) => {
+    return (
+      mag.asesor.toLowerCase().includes(searchQuery) ||
+      mag.cardcode.toLowerCase().includes(searchQuery) ||
+      mag.base.toLowerCase().includes(searchQuery) ||
+      mag.folio_IyD.toString().includes(searchQuery)
+    );
+  });
+
+  if (!mags) return <Loader active inline="centered" />;
+  if (size(mags) === 0) return 'No hay cotizaciones';
 
   return (
-    <div className='list-cotizaciones'>
-        <Search
+    <div className="list-cotizaciones">
+      <Search
         input={{ icon: 'search', iconPosition: 'left' }}
         placeholder="Buscar..."
         value={searchQuery}
         onSearchChange={handleSearchChange}
+        results={filteredMags}
       />
-      <br></br>
-      {map(filteredMags, mag => (
+      <br />
+      {map(filteredMags, (mag) => (
         <InydeItem key={mag._id} mag={mag} onReload={onReload} onClose={onClose} />
       ))}
-        <div className='list-cotizaciones__pagination'>
-            <Pagination
-            totalPages={pagination.total}
-            defaultActivePage={pagination.page}
-            ellipsisItem={null}
-            firstItem={null}
-            lastItem={null}
-            onPageChange={changePage}
-            />
-        </div>
+      <div className="list-cotizaciones__pagination">
+        <Pagination
+          totalPages={pagination.total}
+          defaultActivePage={pagination.page}
+          ellipsisItem={null}
+          firstItem={null}
+          lastItem={null}
+          onPageChange={changePage}
+        />
+      </div>
     </div>
-  )
+  );
 }
