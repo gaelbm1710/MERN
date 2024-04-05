62395e8bbc564c8607c48a0aeeb72616320bf4ac
SoporteS1st3m4as2 <150272012+SoporteS1st3m4as2@users.noreply.github.com>
2024-04-05 16:23:41 -0600

    Update ListInyde.js
    Cambios Search

--- src/components/Admin/Inyde/ListInyde/ListInyde.js
+++ src/components/Admin/Inyde/ListInyde/ListInyde.js
@@ -38,7 +38,7 @@ export function ListInyde(props) {
         mag.asesor.toLowerCase().includes(searchQuery.toLowerCase()) ||
         mag.cardcode.toLowerCase().includes(searchQuery.toLowerCase()) ||
         mag.base.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        mag.folio_IyD.toString().includes(searchQuery.toString())
+        (mag.folio_IyD && mag.folio_IyD.toString().includes(searchQuery.toString()))
       ));
       setFilteredMags(filtered);
     };
