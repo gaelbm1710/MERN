e7bf6d5636b71efb58b6d35726eb17d63e2620a4
DoporteS1st3m4s <soporte.sistemas@o-lab.mx>
2024-04-12 12:04:40 -0600

    V.0.80

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,11 @@
 
 export const ENV = {
     //Conexion prod
-   //BASE_PATH: `https://kaapa-backend.azurewebsites.net/`, 
-   //BASE_API: `https://kaapa-backend.azurewebsites.net/api/v1`,
+   BASE_PATH: `https://kaapa-backend.azurewebsites.net/`, 
+   BASE_API: `https://kaapa-backend.azurewebsites.net/api/v1`,
     //Conexion Local
-    BASE_PATH: `http://localhost:8080`,
-    BASE_API: `http://localhost:8080/api/v1`,
+    //BASE_PATH: `http://localhost:8080`,
+    //BASE_API: `http://localhost:8080/api/v1`,
     API_ROUTES:{
         REGISTER: "auth/register",
         LOGIN: "auth/login",
