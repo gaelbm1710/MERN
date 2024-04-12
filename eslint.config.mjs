662c30872761e4e54755808785f4d775bdcdaf3c
DoporteS1st3m4s <soporte.sistemas@o-lab.mx>
2024-04-12 08:30:20 -0600

    V.0.63

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,18 @@
-module.exports = {
-  env:{
-    browser: true,
-    es2021: true
-  },
-  extends:[
-    'plugin:react/recoommended',
-    'standar'
-  ],
-  parserOptions:{
-    ecmaFeatures:{
-      jsx:true
-    },
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  plugins:[
-    'react'
-  ],
-  rules: {
-    "no-unused-vars": ["error", {
-      "vars": "all",
-      "args": "after-used",
-      "caughtErrors": "all",
-      "ignoreRestSiblings": false,
-      "reportUsedIgnorePattern": false
-  }]
+// eslint.config.js
+import js from "@eslint/js";
+
+export default [
+    js.configs.recommended,
+
+    {
+      "rules": {
+          "no-unused-vars": ["error", {
+              "vars": "all",
+              "args": "after-used",
+              "caughtErrors": "all",
+              "ignoreRestSiblings": false,
+              "reportUsedIgnorePattern": false
+          }]
+      }
   }
-}
+];
