6b06384f45291223ffbc3142924c37445a1678a2
DoporteS1st3m4s <soporte.sistemas@o-lab.mx>
2024-04-12 09:50:07 -0600

    V.0.67

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,34 @@
 module.exports = {
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-      },
+  root: true, 
+  languageOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    extends: [
-      'react-app',
-      'react-app/jest'
-    ],
-    plugins: [
-      'react'
-    ],
-    overrides: [
-      {
-        files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
-        languageOptions: {
-          ecmaVersion: 2021,
-          sourceType: 'module',
-          ecmaFeatures: {
-            jsx: true,
-          },
-        },
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  environments: {
+    browser: true,
+    jest: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  plugins: [
+    'react'
+  ],
+  overrides: [
+    {
+      files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+      rules: {
+        "no-unused-vars": "off",
+        "react-hooks/exhaustive-deps": "warn" 
       },
-    ],
-    rules: {
-      "no-unused-vars": "off"
     }
-  }
-  
+  ],
+};
