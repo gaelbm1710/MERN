6ceddeb25024d281a29dfd1032f98f422075f120
DoporteS1st3m4s <soporte.sistemas@o-lab.mx>
2024-04-12 10:30:21 -0600

    V.0.68/

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,5 @@
 module.exports = {
-  root: true, 
-  languageOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  environments: {
-    browser: true,
-    jest: true,
-  },
-  extends: [
-    'react-app',
-    'react-app/jest'
-  ],
-  plugins: [
-    'react'
-  ],
-  overrides: [
-    {
-      files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
-      rules: {
-        "no-unused-vars": "off",
-        "react-hooks/exhaustive-deps": "warn" 
-      },
-    }
-  ],
+  rules: {
+      "no-unused-vars": "off",
+  }
 };
