b7d719eded03e96d517b8b361f9cc04c5a5cceda
DoporteS1st3m4s <soporte.sistemas@o-lab.mx>
2024-04-15 14:41:40 -0600

    V.0.89

--- src/components/Admin/Menu/MenuForm/MenuForm.js
+++ src/components/Admin/Menu/MenuForm/MenuForm.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Form, Dropdown, Input } from "semantic-ui-react";
 import { useFormik } from "formik";
-import { initialValues,validationSchema} from "./MenuForm.form";
-import {Menu} from "../../../../api";
-import {useAuth} from "../../../../hooks";
+import { initialValues, validationSchema } from "./MenuForm.form";
+import { Menu } from "../../../../api";
+import { useAuth } from "../../../../hooks";
 
 const options = [
   { key: "https://", text: "https://", value: "https://" },
@@ -15,28 +15,28 @@ const menuController = new Menu();
 
 export function MenuForm(props) {
   const { onClose, onReload, menu } = props;
-  const {accessToken} = useAuth();
+  const { accessToken } = useAuth();
   const formik = useFormik({
     initialValues: initialValues(menu),
     validationSchema: validationSchema(),
     validateOnChange: false,
     onSubmit: async (formValue) => {
       try {
-        const data={
+        const data = {
           title: formValue.title,
           path: `${formValue.protocol}${formValue.path}`,
           order: formValue.order,
           active: formValue.active,
         };
-        if(menu){
+        if (menu) {
           data.path = formValue.path;
           await menuController.updateMenu(accessToken, menu._id, data);
-        }else{
+        } else {
           await menuController.createMenu(accessToken, data);
         };
         onClose();
         onReload();
-      }catch (error) {
+      } catch (error) {
         console.error(error);
       }
     }
@@ -46,16 +46,16 @@ export function MenuForm(props) {
   return (
     <Form onSubmit={formik.handleSubmit}>
       <Form.Group widths='equal'>
-        <Form.Input name="title" placeholder="Titulo del Menu" onChange={formik.handleChange} value={formik.values.title} error={formik.errors.title}/>
-        <Form.Input name="order" type="number" placeholder="Orden" onChange={formik.handleChange} value={formik.values.order} error={formik.errors.order}/>
+        <Form.Input name="title" placeholder="Titulo del Menu" onChange={formik.handleChange} value={formik.values.title} error={formik.errors.title} />
+        <Form.Input name="order" type="number" placeholder="Orden" onChange={formik.handleChange} value={formik.values.order} error={formik.errors.order} />
       </Form.Group>
       <Input name="path" placeholder="URL" fluid onChange={formik.handleChange} value={formik.values.path} error={formik.errors.path}
-      label={!menu? <Dropdown options={options} onChange={(_,data)=>formik.setFieldValue("protocol",data.value)} 
-      value={formik.values.protocol} error={formik.errors.protocol}/> 
-      : null }/>
+        label={!menu ? <Dropdown options={options} onChange={(_, data) => formik.setFieldValue("protocol", data.value)}
+          value={formik.values.protocol} error={formik.errors.protocol} />
+          : null} />
       <Form.Group />
       <Form.Button type='submit' primary fluid loading={formik.isSubmitting}>
-        {menu ? "Actualizar Menu": "Crear Menu"}
+        {menu ? "Actualizar Menu" : "Crear Menu"}
       </Form.Button>
     </Form>
   );
--- src/components/Admin/Post/PostForm/PostForm.js
+++ src/components/Admin/Post/PostForm/PostForm.js
@@ -1,94 +1,94 @@
-import React, {useCallback} from 'react';
+import React, { useCallback } from 'react';
 import "./PostForm.scss";
-import {Form, Image} from "semantic-ui-react"
-import {useDropzone} from "react-dropzone"
-import {useFormik} from "formik";
-import {initialValues, validationSchema} from "./PostForm.form";
-import {Editor} from "@tinymce/tinymce-react";
-import {Post} from "../../../../api"
-import {useAuth} from "../../../../hooks"
-import {ENV} from "../../../../utils"
+import { Form, Image } from "semantic-ui-react"
+import { useDropzone } from "react-dropzone"
+import { useFormik } from "formik";
+import { initialValues, validationSchema } from "./PostForm.form";
+import { Editor } from "@tinymce/tinymce-react";
+import { Post } from "../../../../api"
+import { useAuth } from "../../../../hooks"
+import { ENV } from "../../../../utils"
 
 const postController = new Post();
 
 export function PostForm(props) {
-  const {onClose, onReload, post} = props;
-  const {accessToken} = useAuth();
-  const formik=useFormik({
+  const { onClose, onReload, post } = props;
+  const { accessToken } = useAuth();
+  const formik = useFormik({
     initialValues: initialValues(post),
     validationSchema: validationSchema(),
     validateOnChange: false,
-    onSubmit: async(formValue)=>{
+    onSubmit: async (formValue) => {
       try {
-          if(post){
-            await postController.updatePost(accessToken, post._id, formValue);
-          }else{
-            await postController.createPost(accessToken, formValue);
-          }
-          onReload();
-          onClose();
+        if (post) {
+          await postController.updatePost(accessToken, post._id, formValue);
+        } else {
+          await postController.createPost(accessToken, formValue);
+        }
+        onReload();
+        onClose();
       } catch (error) {
-          console.error(error)
+        console.error(error)
       }
     }
   });
 
-  const onDrop = useCallback((acceptedFiles) =>{
+  const onDrop = useCallback((acceptedFiles) => {
     const file = acceptedFiles[0];
     formik.setFieldValue("miniature", URL.createObjectURL(file))
-    formik.setFieldValue("file",file);
-});
+    formik.setFieldValue("file", file);
+  });
 
-const {getRootProps, getInputProps} = useDropzone({
-  accept: "image/jpg, image/png",
-  onDrop
-});
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/jpg, image/png",
+    onDrop
+  });
 
-const getMiniature = () =>{
-  if(formik.values.file){
+  const getMiniature = () => {
+    if (formik.values.file) {
       return formik.values.miniature;
-  }else if(formik.values.miniature){
-    return `${ENV.BASE_PATH}/${formik.values.miniature}`;
-  }
-  return null;
-};
+    } else if (formik.values.miniature) {
+      return `${ENV.BASE_PATH}/${formik.values.miniature}`;
+    }
+    return null;
+  };
 
   return (
     <Form className='post-form' onSubmit={formik.handleSubmit}>
       <Form.Group widths="equal">
-        <Form.Input name="title" placeholder="Titulo del Post" onChange={formik.handleChange} value={formik.values.title} error={formik.errors.title}/>
-        <Form.Input name="path" placeholder="Enlace del Post" onChange={formik.handleChange} value={formik.values.path} error={formik.errors.path}/>
+        <Form.Input name="title" placeholder="Titulo del Post" onChange={formik.handleChange} value={formik.values.title} error={formik.errors.title} />
+        <Form.Input name="path" placeholder="Enlace del Post" onChange={formik.handleChange} value={formik.values.path} error={formik.errors.path} />
       </Form.Group>
 
       <Editor
-      apiKey='mxv7de660tfewx6wboe0balixi3w5ejlhx6zcm0k4mrwdwa3'
-      init={{
-        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
-        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-        tinycomments_mode: 'embedded',
-        tinycomments_author: 'Author name',
-        mergetags_list: [
-          { value: 'First.Name', title: 'First Name' },
-          { value: 'Email', title: 'Email' },
-        ],
-        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
-      }}
-      initialValue={formik.values.content}
-      onBlur={(e) => formik.setFieldValue("content",e.target.getContent())}
-    />
+        apiKey='mxv7de660tfewx6wboe0balixi3w5ejlhx6zcm0k4mrwdwa3'
+        init={{
+          plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+          toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+          tinycomments_mode: 'embedded',
+          tinycomments_author: 'Author name',
+          mergetags_list: [
+            { value: 'First.Name', title: 'First Name' },
+            { value: 'Email', title: 'Email' },
+          ],
+          ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+        }}
+        initialValue={formik.values.content}
+        onBlur={(e) => formik.setFieldValue("content", e.target.getContent())}
+      />
 
 
-      {}
+      { }
       <div className='post-form__miniature' {...getRootProps()}>
-        <input {...getInputProps()}/>
+        <input {...getInputProps()} />
         {getMiniature() ? (
-                <Image size='small' src={getMiniature()}/>
-            ) : (
-                <div>
-                    <span>Arrastra la Imagen</span>
-                </div>
-            )
-            }      
+          <Image size='small' src={getMiniature()} />
+        ) : (
+          <div>
+            <span>Arrastra la Imagen</span>
+          </div>
+        )
+        }
       </div>
       <Form.Button type='submit' primary fluid loading={formik.isSubmitting}>
         {post ? "Actualziar Post" : "Crear Post"}
