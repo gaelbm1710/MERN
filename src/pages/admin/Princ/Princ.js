5b7d28862e489fb7e111520e09d283204989561e
SoporteS1st3m4as2 <150272012+SoporteS1st3m4as2@users.noreply.github.com>
2024-04-24 12:48:15 -0600

    Update Princ.js

--- src/pages/admin/Princ/Princ.js
+++ src/pages/admin/Princ/Princ.js
@@ -12,7 +12,7 @@ export function Princ() {
             <h4>Unificando flujos, simplificando procesos</h4>
             <h3>¡Hola {Nombre} {Apellido}!</h3>
 
-            <h4>Kaapa es una pagina que realizamos para ayudarte a digitalizr y encapsular algunos y proximamente otros flujos de trabajo.</h4>
+            <h4>Kaapa es una pagina que realizamos para ayudarte a digitalizar y encapsular algunos y proximamente otros flujos de trabajo.</h4>
             <h4>Esperemos esto sea de tu agrado y si tienes más sugerencias de otro flujo de trabajo sin problema puedes acercarte al equipo de sistemas.</h4>
         </div>
 
