bd7efe3d3a68e0e3302feea8636f699743f42577
SoporteS1st3m4s <soporte.sistemas@o-lab.mx>
2024-06-15 12:18:20 -0600

    Deploy 15/06/2024 12:18

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const ENV = {
     BASE_API: `https://kaapa-backend.azurewebsites.net/api/v1`,
     //Conexion Local 
     //BASE_PATH: `http://localhost:8080`,
-    //BASE_API: `http://localhost:8080/api/v1`,
+    //BASE_API: `http://localhost:8080/api/v1`, 
     //Contenedor
     USUSARIOS: `https://kaapauploads.blob.core.windows.net`,
     DEFAULT: 'https://kaapauploads.blob.core.windows.net/avatar/default.jpg',
